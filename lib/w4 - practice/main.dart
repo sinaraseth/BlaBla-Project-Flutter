e51d6b6185ce9dd8d5cd278c98f04190b74cf0ca
sinraseth <sinraseth588@gmail.com>
2025-03-07 04:03:13 +0700

    BLA-102 BLA-103 Refactor Rides Service

--- lib/w4 - practice/main.dart
+++ lib/w4 - practice/main.dart
@@ -5,8 +5,8 @@ import 'package:week_3_blabla_project/w4 - practice/service/ride_prefs_service.d
 import 'package:week_3_blabla_project/w4 - practice/theme/theme.dart';
 import 'package:week_3_blabla_project/w4 - practice/repository/mock/mock_locations_repository.dart';
 import 'package:week_3_blabla_project/w4 - practice/service/locations_service.dart';
-import 'package:week_3_blabla_project/w4 - practice/repository/mock/mock_ride_preferences_repository.dart';
 import 'package:week_3_blabla_project/w4 - practice/service/rides_service.dart';
+import 'package:week_3_blabla_project/w4%20-%20practice/repository/mock/mock_rides_resonsitory.dart';
 
 
 void main() {
@@ -14,6 +14,7 @@ void main() {
   // 1 - Initialize the services
   RidePrefService.initialize(MockRidePreferencesRepository());
   LocationsService.initialize(MockLocationsRepository());
+  RidesService.initialize(MockRidesRepository());
 
   // 2- Run the UI
   runApp(const MyApp());
